6b8fb906c8e194dd9d6b03bd33079a4045738e79
Memai2023 <maria.hong26@gmail.com>
2024-02-27 14:11:42 +0100

    birds.js connected

--- routes/birds.js
+++ routes/birds.js
@@ -4,6 +4,7 @@ const birdsRouter = express.Router();
 
 birdsRouter.get('/', (req, res) => res.render("pages/home", {
     pageTitle: "This is the birds page",
+    footer: ""
 }));
 
 export default birdsRouter;
