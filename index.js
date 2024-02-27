d95efb61bf63ec6dd4d75e4e0c8c38bc9434580d
Memai2023 <maria.hong26@gmail.com>
2024-02-27 08:33:02 +0100

    added path, const __dirname, app.set views, made home visible

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import * as path from "path";
+
 const app = express();
 const port = 3050;
+const __dirname = path.resolve();
 
 app.get('/', (req, res) => {
-    res.send("Homepage")
-})
+    res.render("pages/home", {
+
+    })
+});
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.listen(port, console.log(`Listen to ${port}`));
