6bd450cd2855300255cd1a54242012655fa6af2e
Memai2023 <maria.hong26@gmail.com>
2024-02-27 20:43:47 +0100

    adding menu styling in css, create navmenu, sidebar and some changes in header.css

--- routes/birds.js
+++ routes/birds.js
@@ -3,7 +3,7 @@ import express from "express";
 const birdsRouter = express.Router();
 
 birdsRouter.get('/', (req, res) => res.render("pages/home", {
-    pageTitle: "This is the birds page",
+    pageTitle: "Birds",
     footer: "Not copyrighted 2024",
 }));
 
