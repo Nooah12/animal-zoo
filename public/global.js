9fc6e82d425579f3c64060f7243e373818f1017c
Memai2023 <maria.hong26@gmail.com>
2024-02-28 13:32:54 +0100

    added js for navbar in global.js

--- public/global.js
+++ public/global.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const showSideMenu = () => {
+    const sidemenu = document.querySelector('.sidemenu')
+    sidemenu.style.display = 'flex'
+}
+const hideSideMenu= () => {
+    const sidemenu = document.querySelector('.sidemenu')
+    sidemenu.style.display = 'none'
+}
