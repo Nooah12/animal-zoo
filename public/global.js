925e852bb9a5378b08cdcb9aded346614475dc2e
Memai2023 <maria.hong26@gmail.com>
2024-02-28 16:07:41 +0100

    sidbar changes

--- public/global.js
+++ public/global.js
@@ -6,3 +6,19 @@ const hideSideMenu= () => {
     const sidemenu = document.querySelector('.sidemenu')
     sidemenu.style.display = 'none'
 }
+
+document.addEventListener('DOMContentLoaded', function() { 
+    document.querySelectorAll('.sidebar-animal').forEach(item => { 
+        item.addEventListener('click', event => {
+            const clickedSpecies = event.target.textContent;
+            const targetlnfo = document.querySelector(`.animals-container.${clickedSpecies}`); 
+            if (targetlnfo) { 
+                document. querySelectorAll('.animals-container').forEach(info => {
+                    info. style.display = 'none' ; 
+                });
+                targetlnfo.style.display = targetlnfo.style.display  === 'block' ? 'none' : 'block' ; 
+                //document . querySelector('.welcome-message' ) .classList.add('hidden' );
+            }
+        });
+    });
+}); 
