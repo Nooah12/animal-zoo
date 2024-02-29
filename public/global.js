127cd4b78f50a2c02ceabbd170dccf1e6eb1c02e
Memai2023 <maria.hong26@gmail.com>
2024-02-29 15:46:13 +0100

    fixed welcometoggle

--- public/global.js
+++ public/global.js
@@ -7,18 +7,33 @@ const hideSideMenu= () => {
     sidemenu.style.display = 'none'
 }
 
+const getAnimalName = () => {
+    const url = window.location.href;
+    const matches = url.match(/\/animals\/([^?]+)/);
+    if (matches && matches.length > 1) {
+        return matches[1];
+    } else {
+        return null;
+    }
+};
+
+/*const updateLink = (animalName) => {
+    const link = document.querySelector(`.sidebar-animal.${animalName}`);
+
+}*/
+
 document.addEventListener('DOMContentLoaded', function() { 
+    const animalName = getAnimalName();
+    console.log("text"+ animalName);
     document.querySelectorAll('.sidebar-animal').forEach(item => { 
-        item.addEventListener('click', event => {
-            document.querySelectorAll('.sidebar-animal').forEach(otherItem => {
-                if (otherItem !== item) {
-                    otherItem.classList.remove('active');
-                }
-            });
-            item.classList.toggle('active');
-            const clickedSpecies = event.target.textContent;
-            displayAnimalSummary(clickedSpecies);
-        });
+        if (item.classList.contains(animalName)) {
+            item.classList.add("active");
+            const anchor = item.querySelector('a');
+            anchor.href = "/";
+   //         updateLink(animalName);
+        } else {
+            item.classList.remove("active");
+        }
     });
 });
 
